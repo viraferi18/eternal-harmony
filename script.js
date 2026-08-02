8bc7e735eb1d0a92b541ee614a3ba8318e98cd7f
viraferi18 <virafebriana947@gmail.com>
2026-08-02 21:03:53 +0700

    Implement loader and scroll animations
    Add loader functionality and scroll animation effects.

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 
+});
+
+});/* ===========================
+LOADER
+=========================== */
+
+window.addEventListener("load",()=>{
+
+setTimeout(()=>{
+
+loader.style.opacity="0";
+
+setTimeout(()=>{
+
+loader.style.display="none";
+
+},800);
+
+},1200);
+
+});
+
+/* ===========================
+SCROLL ANIMATION
+=========================== */
+
+const fade=document.querySelectorAll(".fade-up");
+
+window.addEventListener("scroll",()=>{
+
+fade.forEach(item=>{
+
+const top=item.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+item.classList.add("show");
+
+}
+
+});
+
+});
